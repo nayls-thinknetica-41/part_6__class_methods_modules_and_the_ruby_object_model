0c1064633b38e793be3c68a49d29efbcbae35921
Mr. Nayls <nayls@bk.ru>
2022-10-13 12:57:15 +0300

    chore: update spec train

--- lib/railway.rb
+++ lib/railway.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative 'railway/train'
-require_relative 'railway/wagon'
-require_relative 'railway/route'
 require_relative 'railway/station'
+require_relative 'railway/route'
+require_relative 'railway/wagon'
+require_relative 'railway/train'
--- spec/railway/train/train_cargo_spec.rb
+++ spec/railway/train/train_cargo_spec.rb
@@ -32,7 +32,7 @@ describe ::Railway::Train::TrainCargo do
       expect { ::Railway::Train::TrainCargo.new }.to raise_error(ArgumentError)
     end
 
-    specify 'только @number обязательный' do
+    specify 'только @param number обязательный' do
       expect(train_default).to be_an_instance_of(::Railway::Train::TrainCargo)
     end
 
@@ -193,28 +193,53 @@ describe ::Railway::Train::TrainCargo do
     end
   end
 
+  context '#route' do
+    specify 'может получить маршрут' do
+      expect(train_with_full_route.route.routes).to eq([station_st1, station_st3, station_st2])
+    end
+
+    specify 'маршрут корректного типа' do
+      expect(train_with_full_route.route).to be_an_instance_of(::Railway::Route)
+    end
+
+    specify 'поезд при назначении маршрута перемещается на первую станцию' do
+      expect(train_with_full_route.current_station).to eq({ index: 0, station: station_st1 })
+    end
+  end
+
+  context '#route_status' do
+    specify 'выводит предыдущую, текущую и следующую станцию' do
+      expect(train_with_default_route.route_status)
+        .to eq({
+                 previous_station: nil,
+                 current_station: station_st1,
+                 next_station: station_st2
+               })
+    end
+  end
+
   context 'move train' do
     context '#forward' do
       specify 'поезд перемещается на следующую станцию по маршруту' do
-        expect(train_with_default_route.forward.current_station)
+        expect(train_with_full_route.forward.current_station)
           .to eq({
                    index: 1,
-                   station: station_st2
+                   station: station_st3
                  })
       end
 
       specify 'поезд никуда не перемещается если нет следующей станции' do
-        expect(train_with_default_route.forward.forward.forward.current_station)
+        expect(train_with_full_route.forward.forward.forward.current_station)
           .to eq({
-                   index: 1,
+                   index: 2,
                    station: station_st2
                  })
       end
 
       specify 'изменяется статус поездки на новую станцию' do
-        expect(train_with_default_route.forward.route_status)
+        expect(train_with_full_route.forward.forward.forward.route_status)
           .to eq({
-                   previous_station: station_st1,
+                   previous_station: station_st3,
                    current_station: station_st2,
                    next_station: nil
                  })
@@ -222,21 +247,18 @@ describe ::Railway::Train::TrainCargo do
     end
 
     context '#backward' do
-      let(:train_with_end_default_route) {
-        train_default.route = ::Railway::Route.new(station_st1, station_st2)
-        train_default.forward.forward
-      }
+      let(:train_end_with_full_route) { train_with_full_route.forward.forward }
 
       specify 'поезд перемещается на предыдущую станцию по маршруту' do
-        expect(train_with_end_default_route.backward.current_station)
+        expect(train_end_with_full_route.backward.current_station)
           .to eq({
-                   index: 0,
-                   station: station_st1
+                   index: 1,
+                   station: station_st3
                  })
       end
 
       specify 'поезд никуда не перемещается если нет предыдущей станции' do
-        expect(train_with_end_default_route.backward.backward.backward.current_station)
+        expect(train_end_with_full_route.backward.backward.backward.current_station)
           .to eq({
                    index: 0,
                    station: station_st1
@@ -244,10 +266,10 @@ describe ::Railway::Train::TrainCargo do
       end
 
       specify 'изменяется статус поездки на новую станцию' do
-        expect(train_with_end_default_route.backward.route_status)
+        expect(train_end_with_full_route.backward.route_status)
           .to eq({
-                   previous_station: nil,
-                   current_station: station_st1,
+                   previous_station: station_st1,
+                   current_station: station_st3,
                    next_station: station_st2
                  })
       end
